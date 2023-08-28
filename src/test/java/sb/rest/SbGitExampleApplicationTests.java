54e0e6c2afe8b490dbd0c8baeebf181d21694684
SandeepReddy <ksandeepreddy2003@gmail.com>
2023-08-28 16:15:46 +0530

    commit

---
+++ src/test/java/sb/rest/SbGitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package sb.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbGitExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
